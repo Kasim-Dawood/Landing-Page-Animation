245f9f3e486d48de1493c236695df5c3f3f8a6c1
Dahoud Kasim <kasimdahoud@gmail.com>
2022-03-25 13:40:18 +0100

    animation ready first try

---
+++ js/statistics.js
@@ -0,0 +1,29 @@
+// Start Statistics section JS    [ 2 ]
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  if (window.scrollY >= statsSection.offsetTop - 500) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+
+
+
+// End Statistics section JS  
